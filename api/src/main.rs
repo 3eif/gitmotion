572b4be47fcb35dc923d9b42c5427e823046f931
Seif <seifaziz10@gmail.com>
2024-08-23 11:08:43 -0400

    fix(api): shorten max duration

--- api/src/main.rs
+++ api/src/main.rs
@@ -371,7 +371,7 @@ fn count_days_and_commits(repo_path: &Path) -> Result<(i32, i32), GourceError> {
 
 fn calculate_seconds_per_day(days_with_commits: i32) -> f64 {
     const MIN_DURATION: f64 = 60.0;
-    const MAX_DURATION: f64 = 120.0;
+    const MAX_DURATION: f64 = 100.0;
     const THRESHOLD: i32 = 1000;
 
     let target_duration = if days_with_commits <= THRESHOLD {
