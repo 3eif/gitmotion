d1188014035f14b7d89afd1570e78b5545ca43ca
Seif <seifaziz10@gmail.com>
2024-08-25 20:37:27 -0400

    perf(api): make duration of generation shorter

--- api/src/main.rs
+++ api/src/main.rs
@@ -533,8 +533,8 @@ fn count_days_and_commits(
 }
 
 fn calculate_seconds_per_day(days_with_commits: i32, job_id: Option<&str>) -> f64 {
-    const MIN_DURATION: f64 = 50.0;
-    const MAX_DURATION: f64 = 90.0;
+    const MIN_DURATION: f64 = 40.0;
+    const MAX_DURATION: f64 = 80.0;
     const THRESHOLD: i32 = 1000;
 
     let target_duration = if days_with_commits <= THRESHOLD {
